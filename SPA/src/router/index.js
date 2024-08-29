b4fa0231d7db053e5aadbc76480e7d711fa317ad
Quentin Peris <quentin.peris@icloud.com>
2024-08-29 09:51:48 +0200

    Ajout de la route Contact

--- SPA/src/router/index.js
+++ SPA/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import testPage from "@/pages/testPage.vue";
+import Contact from "@/pages/Contact.vue";
 
 const router = createRouter({
 	routes: [
-		{ path: '/testpage', component: testPage }],
+		{ path: '/testpage', component: testPage },
+		{ path: '/contact', component: Contact }],
 	history: createWebHistory()
 })
 
